a4c2ce8018700df4181161349d9052516b4037fc
Judy Cha <mjjcha@gmail.com>
2019-05-15 01:14:42 -0700

    Update database.rb

--- config/database.rb
+++ config/database.rb
@@ -4,15 +4,6 @@ configure do
     ActiveRecord::Base.logger = Logger.new(STDOUT)
   end
 
-  set :database, {
-    adapter: "postgresql",
-    host: "ec2-54-83-20-177.compute-1.amazonaws.com",
-    database: "d9483dc6r7lane",
-    user: "hzepgsltzgjbfy",
-    password: "I1pSPZrChRsTDgqKQosIge3idE",
-    port: "5432"
-  }
-
   Dir[APP_ROOT.join('app', 'models', '*.rb')].each do |model_file|
     filename = File.basename(model_file).gsub('.rb', '')
     autoload ActiveSupport::Inflector.camelize(filename), model_file
